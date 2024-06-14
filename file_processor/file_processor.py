60cce2daa6e3a746403d1f6bf25c047e4d05a184
joshuajong1 <joshua.jong@talenox.com>
2024-06-14 11:46:02 +0800

    refactor and clean up code

--- file_processor/file_processor.py
+++ file_processor/file_processor.py
@@ -1,4 +1,13 @@
 import pandas as pd
+import streamlit as st
+from io import BytesIO
+from openpyxl import load_workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+import shutil
+
+def get_uploaded_file():
+  uploaded_file = st.file_uploader("Choose a file", type=["xlsx", "xls", "csv"])
+  return uploaded_file
 
 # Read the Excel file and sample rows of data
 def read_excel_and_sample(filename, sheet_name=0, max_rows=10):
@@ -22,9 +31,40 @@ def extract_headers_from_excel_file(filename, sheet_name=0):
   return headers
 
 # Write to Excel file
-def write_to_excel(result_df, output_path='sampled_output.xlsx'):
-  with pd.ExcelWriter(output_path) as writer:
-    result_df.to_excel(writer, index=False, sheet_name='Sheet1')
+def write_to_preformatted_excel(data, mappings, headers, country):
+    # Load the preformatted Excel file
+    template_path = f"tlx_import_sheet_samples/{country}.xlsx"
+    temp_path = f"temp_{country}.xlsx"
+    shutil.copy(template_path, temp_path)
+
+    # Load the copied Excel file
+    wb = load_workbook(temp_path)
+    ws = wb.active
+    # Create a DataFrame from the data
+    df = pd.DataFrame(data)
+    # Rename the columns based on mappings
+    df = df.rename(columns=mappings)
+    # Ensure all required headers are present, filling with empty strings if missing
+    for header in headers:
+        if header not in df.columns:
+            df[header] = ''
+    # Reorder columns to match the headers
+    df = df[headers]
+    # Write the DataFrame to the copied Excel file
+    for row in dataframe_to_rows(df, index=False, header=False):
+        ws.append(row)
+    # Save the file
+    output = BytesIO()
+    wb.save(output)
+    output.seek(0)
+
+    file_name = f"{country.lower()}.xlsx"
+    st.download_button(
+        label="Download Processed File",
+        data=output,
+        file_name=file_name,
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
 
 # Read CSV and sample rows of data
 def read_csv_and_sample(filename, max_rows=10):
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import json
 import streamlit as st
+import warnings
 from llm_mapper.openai import OpenAiMapper
 from file_processor.file_processor import *
 from static_data_generator.tlx_column_header_mapper import *
@@ -15,9 +16,20 @@ api_key = os.environ.get("OPENAI_API_KEY")
 if not api_key:
   raise ValueError("OpenAI API key is not set. Please check your .env file.")
 
-def get_uploaded_file():
-  uploaded_file = st.file_uploader("Choose a file", type=["xlsx", "xls", "csv"])
-  return uploaded_file
+# Suppress openpyxl warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="openpyxl")
+
+####################################################
+COUNTRY_OPTIONS = ['SINGAPORE', 'MALAYSIA', 'HONG KONG', 'INDONESIA', 'GLOBAL']
+
+def extract_file_data(uploaded_file):
+  if uploaded_file.name.endswith('.csv'):
+      sampled_df = read_csv_and_sample(uploaded_file)
+      raw_data_headers = extract_headers_from_csv(uploaded_file)
+  else:
+    sampled_df = read_excel_and_sample(uploaded_file)
+    raw_data_headers = extract_headers_from_excel_file(uploaded_file)
+  return sampled_df, raw_data_headers
 
 def generate_mappings(raw_data_headers, country_specific_tlx_import_sheet_headers):
   # Initialize the OpenAI client
@@ -30,33 +42,54 @@ def generate_mappings(raw_data_headers, country_specific_tlx_import_sheet_header
   response = llm_model.get_response(prompt)
   return response
 
+def display_initial_mappings(initial_mappings_json, country_specific_tlx_import_sheet_headers):
+  if 'corrected_mappings' not in st.session_state:
+    st.session_state.corrected_mappings = {}
+
+  key = 0
+  country_specific_tlx_import_sheet_headers = [""] + country_specific_tlx_import_sheet_headers
+  for user_header, initial_map in initial_mappings_json.items():
+    # Set initial value to the index of initial_map if it exists, else default to 0
+    initial_index = country_specific_tlx_import_sheet_headers.index(initial_map) if initial_map in country_specific_tlx_import_sheet_headers else 0
+    # Create a column layout
+    col1, col2 = st.columns([3, 3])
+    # Add text box for user header in the first column
+    with col1:
+      user_header_input = st.text_input(f"User Header for '{user_header}':", user_header, disabled=True)
+    # Add dropdown for predefined header in the second column
+    with col2:
+      corrected = st.selectbox(f"Predefined Header for '{user_header}':", country_specific_tlx_import_sheet_headers, index=initial_index,key=user_header)
+      key += 1
+    st.session_state.corrected_mappings[user_header_input] = corrected
+  return st.session_state.corrected_mappings
+
 def app():
   st.title("Upload and Process Excel/CSV File")
   uploaded_file = get_uploaded_file()
   if uploaded_file is not None:
-    if uploaded_file.name.endswith('.csv'):
-      sampled_df = read_csv_and_sample(uploaded_file)
-      raw_data_headers = extract_headers_from_csv(uploaded_file)
-    else:
-      sampled_df = read_excel_and_sample(uploaded_file)
-      raw_data_headers = extract_headers_from_excel_file(uploaded_file)
+    sampled_df, raw_data_headers = extract_file_data(uploaded_file)
     st.write("File Uploaded Successfully.")
     st.write(sampled_df)
 
-    country = st.selectbox("Select Country:", options=['SINGAPORE', 'MALAYSIA', 'HONG KONG', 'INDONESIA', 'GLOBAL'])
+    if 'confirmed_country' not in st.session_state:
+      st.session_state.confirmed_country = None
+    country = st.selectbox("Select Country:", options=COUNTRY_OPTIONS)
+    # Add a submit button to confirm the country selection
+    if st.button("Confirm Country"):
+      st.session_state.confirmed_country = country
 
-    if country:
-      country_specific_tlx_import_sheet_headers = get_column_headers(country.lower().replace(" ", "_"))
+    if st.session_state.confirmed_country:
+      country_specific_tlx_import_sheet_headers = get_column_headers(st.session_state.confirmed_country.lower().replace(" ", "_"))
       initial_mappings = generate_mappings(raw_data_headers, country_specific_tlx_import_sheet_headers)
       # initial_mappings = '''
       #   {
-      #     "Employee ID": "Employee ID*",
-      #     "First Name": "First Name",
-      #     "Last Name": "Last Name",
+      #     "Employee ID": "Employee ID",
+      #     "First Name*": "First Name",
+      #     "Last Name*": "Last Name",
       #     "Nickname": "Nickname",
       #     "Chinese Name": "Chinese Name",
-      #     "Email": "Email Address",
-      #     "Invite User": "Invite User*",
+      #     "Email": "Email",
+      #     "Invite User*": "Invite User",
       #     "User Email (if different from employee email)": "User Email (if different from employee email)",
       #     "Access Role": "Access Role",
       #     "My Profile Module": "My Profile Module",
@@ -65,11 +98,51 @@ def app():
       #     "Leave Module": "Leave Module",
       #     "Payroll Module": "Payroll Module",
       #     "Profile Module": "Profile Module",
-      #     "Birth Date (DD/MM/YYYY)": "Birth Date (DD/MM/YYYY)",
+      #     "Birth Date (DD/MM/YYYY)*": "Birth Date (DD/MM/YYYY)",
       #     "Gender": "Gender",
-      #     "Marital Status": "Marital Status",
-      #     "Identification Number": "Identification Number*",
-      #     "Immigration Status": "Immigration Status*",
+      #     "Marital Status*": "Marital Status",
+      #     "Identification Number*": "Identification No",
+      #     "Immigration Status*": "Immigration Status",
+      #     "Disabled Individual*": "N/A (Suggestion: Include the column as needed)",
+      #     "Disabled Spouse*": "N/A (Suggestion: Include the column as needed)",
+      #     "Contributing EPF?*": "N/A (Suggestion: Include the column as needed)",
+      #     "EPF Number*": "N/A (Suggestion: Include the column as needed)",
+      #     "Employee EPF Setting*": "N/A (Suggestion: Include the column as needed)",
+      #     "Employer EPF Setting*": "N/A (Suggestion: Include the column as needed)",
+      #     "PCB No.(Income tax no.)*": "N/A (Suggestion: Include the column as needed)",
+      #     "PCB Borne by Employer*": "N/A (Suggestion: Include the column as needed)",
+      #     "Socso Category*": "N/A (Suggestion: Include the column as needed)",
+      #     "Employment Insurance System(EIS)*": "N/A (Suggestion: Include the column as needed)",
+      #     "Zakat No.": "N/A (Suggestion: Include the column as needed)",
+      #     "Zakat Amount": "N/A (Suggestion: Include the column as needed)",
+      #     "Contributing HRDF?*": "N/A (Suggestion: Include the column as needed)",
+      #     "Passport No": "Passport No.",
+      #     "Passport Date of Issue (DD/MM/YYYY)": "Passport Date of Issue (DD/MM/YYYY)",
+      #     "Passport Date of Expiry (DD/MM/YYYY)": "Passport Date of Expiry (DD/MM/YYYY)",
+      #     "Passport Place of Issue": "Passport Place of Issue",
+      #     "Nationality": "Nationality",
+      #     "Race": "Race",
+      #     "Religion": "Religion",
+      #     "Job Title*": "Job Title",
+      #     "Hired Date (DD/MM/YYYY)*": "Hired Date (DD/MM/YYYY)",
+      #     "Job Start Date (DD/MM/YYYY)": "Job Start Date (DD/MM/YYYY)",
+      #     "Department": "Department",
+      #     "Location/Branch": "Location/Branch",
+      #     "Default Cost Centre": "Default Cost Centre",
+      #     "Role": "Role",
+      #     "Confirmation Date (DD/MM/YYYY)": "Confirmation Date (DD/MM/YYYY)",
+      #     "Working Day*": "Working Day",
+      #     "Working Hour*": "Working Hour",
+      #     "Rate of Pay*": "Rate of Pay",
+      #     "Currency of Salary*": "Currency of Salary",
+      #     "Basic Salary*": "Basic Salary",
+      #     "Designation in Accounting Software": "Designation in Accounting Software",
+      #     "Job Remarks": "Job Remarks",
+      #     "Resign Date (DD/MM/YYYY)": "Resign Date (DD/MM/YYYY)",
+      #     "Payment Method*": "Payment Method",
+      #     "Bank Type": "Bank Type",
+      #     "Bank Account Holders Name": "Bank Account Holder's Name",
+      #     "Bank Account No.": "Bank Account No.",
       #     "Contact Number": "Contact Number",
       #     "Office Direct Inward Dialing (DID) Number": "Office Direct Inward Dialing (DID) Number",
       #     "Address Line 1": "Address Line 1",
@@ -77,41 +150,45 @@ def app():
       #     "Country": "Country",
       #     "Region": "Region",
       #     "Subregion": "Subregion",
-      #     "Postal Code": "Postal Code"
+      #     "Postal Code": "Postal Code",
+      #     "Next of Kins Name": "Next of Kin's Name",
+      #     "Next of Kins Nationality": "Next of Kin's Nationality",
+      #     "Next of Kins Gender": "Next of Kin's Gender",
+      #     "Next of Kins Birth Date (DD/MM/YYYY)": "Next of Kin's Birth Date (DD/MM/YYYY)",
+      #     "Next of Kins Identification No.": "Next of Kin's Identification No.",
+      #     "Next of Kins Passport No.": "Next of Kin's Passport No.",
+      #     "Next of Kins Relationship": "Next of Kin's Relationship",
+      #     "Next of Kins Marriage Date (Spouse) (DD/MM/YYYY)": "Next of Kin's Marriage Date (Spouse) (DD/MM/YYYY)",
+      #     "Next of Kins Contact No.": "Next of Kin's Contact No.",
+      #     "Accumulated remuneration/Benefit-In-Kind (BIK)/Value Of Living Accomodation (VOLA)*": "N/A (Suggestion: Include the column as needed)",
+      #     "Accumulated EPF and Other Approved Funds [include life premium insurance]*": "N/A (Suggestion: Include the column as needed)",
+      #     "Accumulated MTD paid (including MTD on additional remuneration)*": "N/A (Suggestion: Include the column as needed)",
+      #     "Accumulated SOCSO Contribution*": "N/A (Suggestion: Include the column as needed)",
+      #     "Accumulated Zakat paid*": "N/A (Suggestion: Include the column as needed)",
+      #     "Payroll year to start applying accumulated deductions.*": "N/A (Suggestion: Include the column as needed)",
+      #     "Payroll month to start applying accumulated deductions.*": "N/A (Suggestion: Include the column as needed)",
+      #     "Covid-19 Vaccination Status": "Covid-19 Vaccination Status",
+      #     "Covid-19 Vaccine Brand": "Covid-19 Vaccine Brand",
+      #     "Date of 1st Dose (DD/MM/YYYY)": "Date of 1st Dose (DD/MM/YYYY)",
+      #     "Date of 2nd Dose (DD/MM/YYYY)": "Date of 2nd Dose (DD/MM/YYYY)",
+      #     "Covid-19 Vaccine Booster Brand": "Covid-19 Vaccine Booster Brand",
+      #     "Date of Booster Dose (DD/MM/YYYY)": "Date of Booster Dose (DD/MM/YYYY)",
+      #     "Vaccination Remarks": "Vaccination Remarks",
+      #     "Unnamed: 91": "N/A (Suggestion: Include the column as needed)"
       #   }
       # '''
       initial_mappings_cleaned = initial_mappings.replace('\n', '')
       initial_mappings_json = json.loads(initial_mappings_cleaned)
-
       st.write("Please review and correct the mappings:")
-
-      corrected_mappings = {}
-      key = 0
-      country_specific_tlx_import_sheet_headers = [""] + country_specific_tlx_import_sheet_headers
-      for user_header, initial_map in initial_mappings_json.items():
-        # Set initial value to the index of initial_map if it exists, else default to 0
-        initial_index = country_specific_tlx_import_sheet_headers.index(initial_map) if initial_map in country_specific_tlx_import_sheet_headers else 0
-        # Create a column layout
-        col1, col2 = st.columns([3, 3])
-        # Add text box for user header in the first column
-        with col1:
-          user_header_input = st.text_input(f"User Header for '{user_header}':", user_header, disabled=True)
-        # Add dropdown for predefined header in the second column
-        with col2:
-          corrected = st.selectbox(f"Predefined Header for '{user_header}':", country_specific_tlx_import_sheet_headers, index=initial_index,key=key)
-          key += 1
-        corrected_mappings[user_header_input] = corrected
+      corrected_mappings = display_initial_mappings(initial_mappings_json, country_specific_tlx_import_sheet_headers)
 
       if st.button("Submit Mappings"):
         st.write("Final Corrected Mappings:", corrected_mappings)
+        # Read the uploaded file again to get the full data
+        data = pd.read_csv(uploaded_file) if uploaded_file.name.endswith('.csv') else pd.read_excel(uploaded_file)
 
-# Example function call
+        # Write to the preformatted file
+        write_to_preformatted_excel(data, corrected_mappings, country_specific_tlx_import_sheet_headers[1:], st.session_state.confirmed_country)
+
 if __name__ == "__main__":
   app()
-  # display_menu()
-  # user_choice = get_user_choice()
-  # country_specific_tlx_import_sheet_headers = get_column_headers(country=user_choice)
-  # raw_data_headers = "name, birthdate, address"
-  # prompt = llm_model.create_mapping_prompt(raw_data_headers, country_specific_tlx_import_sheet_headers)
-  # response = llm_model.get_response(prompt)
-  # print(response)
